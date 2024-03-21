31791b2b268dd79fd45858e39ca981856c5f60d3
jiangsuihua <jshfsym@sina.com>
2024-03-21 13:16:44 +0800

    i18n转换工具

---
+++ i18n/properties转sql.py
@@ -0,0 +1,28 @@
+keyset = set()
+def generate_insert_statement(key, zh, en=None, sp=None, ru=None, fr=None):
+    insert_statement = "INSERT INTO `shuya`.`i18n` (`wkey`, `zh`, `en`, `sp`, `ru`, `fr`) VALUES "
+    values = f"('{key}', '{zh}', '{en}', '{sp}', '{ru}', '{fr}')"
+    return insert_statement + values + ";"
+
+def read_properties_file(file_path):
+    data = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                parts = line.split('=')
+                key = parts[0].strip()
+                value = '='.join(parts[1:]).strip()  # 将剩余的部分组合成 value
+                data[key] = value
+    return data
+
+properties_file_path = "zh.properties"
+data = read_properties_file(properties_file_path)
+
+for key, value in data.items():
+    value = value.replace("'", "''")  # 处理单引号，以防在 SQL 语句中引起问题
+    if(key not in keyset):
+        print(generate_insert_statement(key, value))
+        keyset.add(key)
+    else:
+        pass
